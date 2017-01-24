db83503b8cddbfcb28a68763e0a6b17a7a97f456
crutch12 <criitch@yandex.ru>
2017-01-24 19:17:15 +0300

    Google Maps Added

---
+++ js/googleMaps.js
@@ -0,0 +1,41 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 55.753, lng: 37.609},
+    zoom: 10
+  });
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      var marker = new google.maps.Marker({
+    position: pos,
+    map: map,
+    title: 'Я здесь'
+  });
+
+      marker.addListener('click', function() {
+    map.setZoom(17);
+    map.setCenter(marker.getPosition());
+  });
+
+      map.setCenter(pos);
+      map.setZoom(14);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
