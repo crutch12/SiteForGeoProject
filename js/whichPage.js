d5c929457b12bc399653347930a813d5a5eaac9c
crutch12 <criitch@yandex.ru>
2017-01-24 16:04:04 +0300

    Lights

--- js/whichPage.js
+++ js/whichPage.js
@@ -1,10 +1,27 @@
 
 var nav =  document.getElementById("navigation");
+
 var links =  nav.getElementsByTagName("a");
+
 var THref = location.href;
-var count = links.length
-for(var i=0; i<count; i++){
-  if(links[i].href==THref){
-  		links[i].parentElement.className += 'active'
+
+var pageName = THref.substr(THref.lastIndexOf('/') + 1);
+var count = links.length;
+
+var specName = ' active';
+
+if (!pageName){
+	var mainPage =  document.getElementById("mainPage");
+	mainPage.className += specName;
+	}else{
+		for(var i=0; i<count; i++){
+	  		if(links[i].href == THref){
+	  			links[i].parentElement.className += specName;
+		}
 	}
 }
+
+// var url = document.createElement('a');
+// url.href = location.pathname;
+// alert(url.pathname + url.search);
+//}
