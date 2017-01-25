bb70a115c5224972a7fb07dbd5a59fb62d2677cd
crutch12 <criitch@yandex.ru>
2017-01-25 15:07:33 +0300

    Nav bar fixed for spa

--- js/whichPage.js
+++ js/whichPage.js
@@ -1,26 +1,49 @@
 
-var nav =  document.getElementById("navigation");
+function WhichPage(){
+	var nav =  document.getElementById("navigation");
 
-var links =  nav.getElementsByTagName("a");
+	var links =  nav.getElementsByTagName("a");
 
-var THref = location.href;
+	//var THref = location.href;
+	//var pageName = THref.substr(THref.lastIndexOf('/') + 1);
 
-var pageName = THref.substr(THref.lastIndexOf('/') + 1);
-var count = links.length;
+	var THref = location.href;
+	var pageName = THref.substr(THref.lastIndexOf('/') + 1);
 
-var specName = 'active';
+	var k = document.getElementsByTagName("li");
 
-if (!pageName){
-	var mainPage =  document.getElementById("mainPage");
-	mainPage.classList.add(specName);
-	}else{
-		for(var i=0; i<count; i++){
-	  		if(links[i].href == THref){
-	  			links[i].parentElement.classList.add(specName);
-	  			//links[i].style.backgroundColor = 'yellow';
-		}
-	}
+	var count = links.length;
+	var specName = 'active';
+     
+	for(var i = 0; i < k.length; i++){
+		k[i].onclick = function (){
+			THref = this.getElementsByTagName('a')[0];
+			pageName = THref.href.substr(THref.href.lastIndexOf('/') + 1);
+
+			//alert(pageName);
+			             
+			if (!pageName){
+				var mainPage = document.getElementById("mainPage");
+				mainPage.classList.add(specName);
+				}else{
+					for(var i=0; i<count; i++){
+					  	if(links[i].href == THref){
+					  		links[i].parentElement.classList.add(specName);
+					  		//links[i].style.backgroundColor = 'yellow';
+						}else{
+							links[i].parentElement.classList.remove(specName);
+						}
+					}
+				}
+            }
+     }          
+
+	
+
+	//alert(pageName);
 }
+	//alert(pageName);
+WhichPage();
 
 // var url = document.createElement('a');
 // url.href = location.pathname;
