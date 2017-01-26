25328cdb1e3bb1b1a4e905738d85010f92798416
crutch12 <criitch@yandex.ru>
2017-01-26 16:43:58 +0300

    update timer

--- js/timer.js
+++ js/timer.js
@@ -5,21 +5,27 @@ var i = 0;
 
 function startTimer() {    
     setTimeout(function() {
-        // your code here...
+
         i++;
         console.log(i);
-        if(document.getElementById('time')){
-        	var time = document.getElementById('time');
-        	time.innerHTML = i;
-        };
+
+        ShowTime();
         // calculate the actual number of ms since last time
         var actual = Date.now() - start;
         // subtract any extra ms from the delay for the next cycle
         thisDelay = myDelay - (actual - myDelay);
         start = Date.now();
         // start the timer again
+
         startTimer();
     }, thisDelay);
 }
 
 startTimer();
+
+function ShowTime(){
+        if(document.getElementById('time')){
+        	var time = document.getElementById('time');
+        	time.innerHTML = i;
+        };
+}
