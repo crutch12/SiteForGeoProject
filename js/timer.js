91ba0c4f632eae38871ac7f6be627be6473e347e
crutch12 <criitch@yandex.ru>
2017-01-26 18:37:37 +0300

    timver v0.8

--- js/timer.js
+++ js/timer.js
@@ -6,25 +6,22 @@ function StartTimer() {
 
 	ShowTime(seconds, time);
 
-    setTimeout(function() {
+    Tic(seconds, time);
+}
+
+function Tic(seconds, time){
+	 setTimeout(function() {
 
         seconds++;
-        //console.log(i);
 
         ShowTime(seconds, time);
-        // calculate the actual number of ms since last time
-        //var actual = Date.now() - start;
-        // subtract any extra ms from the delay for the next cycle
-        //thisDelay = myDelay - (actual - myDelay);
-        //start = Date.now();
-        // start the timer again
 
-        StartTimer();
+        Tic();
     }, 60);
 }
 
 //startTimer();
 
 function ShowTime(seconds, time){
-		time.innerHTML = seconds;  
+	time.innerHTML = seconds; 
 }
