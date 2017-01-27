c52c2d79863c0124ac790f9aab32dd49af41c5ea
crutch12 <criitch@yandex.ru>
2017-01-27 15:15:43 +0300

    resume ready

--- js/timer.js
+++ js/timer.js
@@ -1,22 +1,11 @@
 var start = Date.now();
 
-// var hoursTime;
-// var minutesTime;
-// var secondsTime;
-
-//var times;
-
 function StartTimer() {    
-	//times = Math.floor((Date.now() - start));
-	//if (document.ready){
 	var hoursTime = document.getElementById('hoursTime');
 	var minutesTime = document.getElementById('minutesTime');
 	var secondsTime = document.getElementById('secondsTime');
-	//alert(hoursTime.innerHTML);
     
     Tic(hoursTime, minutesTime, secondsTime);
-	//}
-	
 }
 
 function Tic(hoursTime, minutesTime, secondsTime){
@@ -29,18 +18,12 @@ function Tic(hoursTime, minutesTime, secondsTime){
     }, 30);
 }
 
-//startTimer();
-
 function ShowTime(hoursTime, minutesTime, secondsTime){
 	var passedTime = Math.floor((Date.now() - start) / 1000);
 
 	var hours = Math.floor(passedTime / (60 * 60));
 	var minutes = Math.floor((passedTime - hours * 60 * 60) / 60);
 	var seconds = Math.floor(passedTime - minutes * 60 - hours * 60 * 60);
-
-	// hoursTime.innerHTML = hours;
-	// minutesTime.innerHTML = minutes;
-	// secondsTime.innerHTML = seconds;
 
 	hoursTime.innerHTML = PasteZero(hours);
 	minutesTime.innerHTML = PasteZero(minutes);
@@ -51,6 +34,5 @@ function PasteZero(value){
 	if(value.toString().length < 2){
 		return 0 + value.toString();
 	}
-
 	return value;
 }
