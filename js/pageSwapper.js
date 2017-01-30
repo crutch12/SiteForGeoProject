1bf372635339a8edc91a94b736ab21578937cb6a
crutch12 <criitch@yandex.ru>
2017-01-30 12:50:00 +0300

    huge timer fix

--- js/pageSwapper.js
+++ js/pageSwapper.js
@@ -1,95 +1,80 @@
+function WhichPage() {
+    var nav = document.getElementById("navigation");
 
-function WhichPage(){
-	var nav =  document.getElementById("navigation");
+    var links = nav.getElementsByTagName("a");
 
-	var links =  nav.getElementsByTagName("a");
+    var THref = window.location;
+    var pageName = GetPageName();
 
-	var THref = window.location;
-	var pageName = THref.pathname.substr(THref.pathname.lastIndexOf('/') + 1);
+    var count = links.length;
+    var specName = 'active';
 
-	var k = document.getElementsByTagName("li");
+    if (!pageName) {
 
-	var count = links.length;
-	var specName = 'active';
-	if (!pageName){
+        for (var i = 0; i < count; i++) {
+            links[i].parentElement.classList.remove(specName);
+        }
 
-		for(var i=0; i<count; i++){
-					links[i].parentElement.classList.remove(specName);
-		}
-
-	var mainPage = document.getElementById('mainPage');
-	mainPage.classList.add(specName);
-	}else{
-		for(var i=0; i<count; i++){
-			if(links[i].href == THref){
-				links[i].parentElement.classList.add(specName);
-				}else{
-					links[i].parentElement.classList.remove(specName);
-				}
-			}
-		}
-     
-	for(var i = 0; i < k.length; i++){
-		k[i].onclick = function (){
-			THref = this.getElementsByTagName('a')[0];
-			pageName = THref.href.substr(THref.href.lastIndexOf('/') + 1);
-
-			if (!pageName){
-				var mainPage = document.getElementById("mainPage");
-				mainPage.classList.add(specName);
-				}else{
-					for(var i=0; i<count; i++){
-					  	if(links[i].href == THref){
-					  		links[i].parentElement.classList.add(specName);
-						}else{
-							links[i].parentElement.classList.remove(specName);
-						}
-					}
-				}
+        var mainPage = document.getElementById('mainPage');
+        mainPage.classList.add(specName);
+    } else {
+        for (var i = 0; i < count; i++) {
+            if (links[i].href == THref) {
+                links[i].parentElement.classList.add(specName);
+            } else {
+                links[i].parentElement.classList.remove(specName);
             }
-     }          
+        }
+    }
 }
 
-$(document).ready(function() {
-	         WhichPage();
-    $('a').click(function() {
+function GetPageName() {
+    var THref = window.location;
+    var pageName = THref.pathname.substr(THref.pathname.lastIndexOf('/') + 1);
+
+    return pageName;
+}
+
+$(document).ready(function () {
+    WhichPage();
+    $('a').click(function () {
         var url = $(this).attr('href');
 
         $.ajax({
-            url:     url + '?ajax=1',
-            success: function(result){
-	            var str = JSON.stringify(result);
-				var html = str.replace(/\\n|\\t|\\r|\\\u0022/gi, "");
-				var title = html.match(/<title[^>]*>([^<]+)<\/title>/)[0];
-				var doc = $($.parseXML(title));
-				var textTitle = doc.find('title').text();
-				document.getElementsByTagName('title')[0].innerHTML = textTitle;
-	            $('#containerDiv').html($(result).find('#contentDiv'));
+            url: url + '?ajax=1',
+            success: function (result) {
+                SetHtmlContent(result);
             }
         });
 
-        if(url != window.location){
+        if (url != window.location) {
             window.history.pushState(null, null, url);
         }
         WhichPage();
+        StopTimer(GetPageName());
 
         return false;
     });
 });
 
-$(window).bind('popstate', function() {
-	                        WhichPage();
+$(window).bind('popstate', function () {
+    WhichPage();
+    StopTimer(GetPageName());
     $.ajax({
-        url:     location.pathname + '?ajax=1',
-        success: function(result) {
-	        var str = JSON.stringify(result);
-			var html = str.replace(/\\n|\\t|\\r|\\\u0022/gi, "");
-			var title = html.match(/<title[^>]*>([^<]+)<\/title>/)[0];
-			var doc = $($.parseXML(title));
-			var textTitle = doc.find('title').text();
-			document.getElementsByTagName('title')[0].innerHTML = textTitle;
-            $('#containerDiv').html($(result).find('#contentDiv'));
+        url: location.pathname + '?ajax=1',
+        success: function (result) {
+            SetHtmlContent(result);
         }
-        
+
     });
 });
+
+function SetHtmlContent(result) {
+    var str = JSON.stringify(result);
+    var html = str.replace(/\\n|\\t|\\r|\\\u0022/gi, "");
+    var title = html.match(/<title[^>]*>([^<]+)<\/title>/)[0];
+    var doc = $($.parseXML(title));
+    var textTitle = doc.find('title').text();
+    document.getElementsByTagName('title')[0].innerHTML = textTitle;
+    $('#containerDiv').html($(result).find('#contentDiv'));
+}
--- js/timer.js
+++ js/timer.js
@@ -1,78 +1,86 @@
 var start = Date.now();
-var delay = 1000/50;
+var delay = 1000 / 50;
 var tics = 0;
 
 var secLength = 1000;
 var minLength = secLength * 60;
 var hourLength = minLength * 60;
 
-function StartTimer() {    
-	var hoursTime = document.getElementById('hoursTime');
-	var minutesTime = document.getElementById('minutesTime');
-	var secondsTime = document.getElementById('secondsTime');
+var hoursTime;
+var minutesTime;
+var secondsTime;
 
-    //ShowTime(hoursTime, minutesTime, secondsTime);
+var timer;
+
+var timerPageName = "timer.html";
+
+var timelineSec;
+var timelineMin;
+var timelineHour;
+
+function StartTimer() {
+    hoursTime = document.getElementById('hoursTime');
+    minutesTime = document.getElementById('minutesTime');
+    secondsTime = document.getElementById('secondsTime');
+
+    timelineSec = document.getElementById('timelineSec');
+    timelineMin = document.getElementById('timelineMin');
+    timelineHour = document.getElementById('timelineHour');
+
     Refresh();
-    Tic(hoursTime, minutesTime, secondsTime);
+    timer = setInterval(Tic, delay);
 }
 
-function Tic(hoursTime, minutesTime, secondsTime){
-	 	setTimeout(function() {
-	 		tics++;
-	        ShowTime(hoursTime, minutesTime, secondsTime);
-	        Tic(hoursTime, minutesTime, secondsTime);
-    }, delay);
+function Tic() {
+    ShowTime();
+    tics++;
 }
 
-function ShowTime(hoursTime, minutesTime, secondsTime){
-	var passedTime = Math.floor((Date.now() - start) / secLength);
+function ShowTime() {
+    var passedTime = Math.floor((Date.now() - start) / secLength);
 
-	var hours = Math.floor(passedTime / (60 * 60));
-	var minutes = Math.floor((passedTime - hours * 60 * 60) / 60);
-	var seconds = Math.floor(passedTime - minutes * 60 - hours * 60 * 60);
+    var hours = Math.floor(passedTime / (60 * 60));
+    var minutes = Math.floor((passedTime - hours * 60 * 60) / 60);
+    var seconds = Math.floor(passedTime - minutes * 60 - hours * 60 * 60);
 
-	var timelineSec = document.getElementById('timelineSec');
-	timelineSec.style.width = (((Date.now() - start) % secLength) / secLength) * 100 + '%';
+    timelineSec.style.width = (((Date.now() - start) % secLength) / secLength) * 100 + '%';
+    if (tics % (10 * 2) == 0) {
 
-	if (tics % (10 * 2) == 0){
-		var timelineMin = document.getElementById('timelineMin');
-		timelineMin.style.width = (((Date.now() - start) % minLength) / minLength) * 100 + '%';
-	}
+        timelineMin.style.width = (((Date.now() - start) % minLength) / minLength) * 100 + '%';
+    }
+    if (tics % (10 * 2 * 60) == 0) {
 
-	if (tics % (10 * 2 * 60) == 0){
-		var timelineHour = document.getElementById('timelineHour');
-		timelineHour.style.width = (((Date.now() - start) % hourLength) / hourLength) * 100 + '%';
-	}	
+        timelineHour.style.width = (((Date.now() - start) % hourLength) / hourLength) * 100 + '%';
+    }
 
-	hoursTime.innerHTML = PasteZero(hours);
-	minutesTime.innerHTML = PasteZero(minutes);
-	secondsTime.innerHTML = PasteZero(seconds);
+    hoursTime.innerHTML = PasteZero(hours);
+    minutesTime.innerHTML = PasteZero(minutes);
+    secondsTime.innerHTML = PasteZero(seconds);
 }
 
-function PasteZero(value){
-	if(value.toString().length < 2){
-		return 0 + value.toString();
-	}
-	return value;
+function PasteZero(value) {
+    if (value.toString().length < 2) {
+        return 0 + value.toString();
+    }
+    return value;
 }
 
-window.addEventListener("focus", function(event) { 
-	Refresh();
+window.addEventListener("focus", function (event) {
+    Refresh();
 }, false);
 
-function Refresh(){
-	var hoursTime = document.getElementById('hoursTime');
-	var minutesTime = document.getElementById('minutesTime');
-	var secondsTime = document.getElementById('secondsTime');
+function Refresh() {
+    timelineSec.style.width = (((Date.now() - start) % secLength) / secLength) * 100 + '%';
+    timelineMin.style.width = (((Date.now() - start) % minLength) / minLength) * 100 + '%';
+    timelineHour.style.width = (((Date.now() - start) % hourLength) / hourLength) * 100 + '%';
+}
 
-	if (hoursTime && minutesTime && secondsTime){
-		var timelineSec = document.getElementById('timelineSec');
-		timelineSec.style.width = (((Date.now() - start) % secLength) / secLength) * 100 + '%';
+function StopTimer(pageName) {
+    if (pageName != timerPageName) {
+        TimerStop();
+    }
+}
 
-		var timelineMin = document.getElementById('timelineMin');
-		timelineMin.style.width = (((Date.now() - start) % minLength) / minLength) * 100 + '%';
-
-		var timelineHour = document.getElementById('timelineHour');
-		timelineHour.style.width = (((Date.now() - start) % hourLength) / hourLength) * 100 + '%';
-	}
+function TimerStop() {
+    clearInterval(timer);
 }
